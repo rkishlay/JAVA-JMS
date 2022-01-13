65e2862b5f11e2825569e931248bfaffd622a5b4
Kishlay <Kishlay@gmail.com>
2022-01-13 20:51:16 +0530

    Initial Commit - First

---
+++ src/main/java/com/java/jms/JavaJmsApplication.java
@@ -0,0 +1,13 @@
+package com.java.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/jms/JavaJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
